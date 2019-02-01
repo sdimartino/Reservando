9e1e0ce017fe2ac28719f0699ef57ad40c739f2a
Sandra Di Martino <dimartinosandra@gmail.com>
2019-02-01 17:48:15 -0300

    Agrego casos de test reservarHorario inválido. Casos de test para obtenerPuntuacion y calificar. Agrego readme.md

--- js/testrestaurant.js
+++ js/testrestaurant.js
@@ -21,65 +21,71 @@ describe('Restaurant',function(){
                 var horariosRestantes = restaurante.devolverHorarios();
                 expect(horariosRestantes.length).to.equal(cantHorariosAntesReserva - 1);
             });
+        });
+        describe('inválido', function(){
+            it('por no estar el horario indicado como disponible, el arreglo de horarios no modifica sus elementos.', function(){
+                var cantHorariosAntesReserva = restaurante.devolverHorarios().length;
+                var horariosIniciales = restaurante.devolverHorarios();
+                restaurante.reservarHorario("11:00");
+                expect(restaurante.devolverHorarios().length).to.equal(cantHorariosAntesReserva);
+                expect(restaurante.devolverHorarios()).to.be.equal(horariosIniciales);
+            });
+            it('por no indicar horario alguno, el arreglo de horarios no modifica sus elementos', function(){
+                var horariosIniciales = restaurante.devolverHorarios();
+                var cantHorariosAntesReserva = restaurante.devolverHorarios().length;
+                restaurante.reservarHorario();
+                expect(restaurante.devolverHorarios().length).to.equal(cantHorariosAntesReserva);
+                expect(restaurante.devolverHorarios()).to.equal(horariosIniciales);
+            });
+        });
+    });    
+    describe('obtenerPuntuacion', function(){
+        var restaurante; 
+        beforeEach(() => {
+            restaurante =  new Restaurant(
+                22, 
+                "Byron Hoxton", 
+                "Hamburguesa", 
+                "Londres", 
+                ["14:00", "16:00", "21:30"], 
+                "../img/hamburguesa3.jpg", [1, 9, 10, 10, 5]);
+        });
+        describe(' de las siguientes calificaciones ',function(){
+            it('[1, 9, 10, 10, 5] se obtiene 5', function(){
+                var puntuacion = restaurante.obtenerPuntuacion();
+                expect(puntuacion).to.equal(7);
+            });
+            it('[] se obtiene 0', function(){
+                restaurante.calificaciones = [];
+                var puntuacion = restaurante.obtenerPuntuacion();
+                expect(puntuacion).to.equal(0);
+            });
+            it('[3, 3, 4] se obtiene 3.33', function(){
+                restaurante.calificaciones = [3, 3, 4];
+                var puntuacion = restaurante.obtenerPuntuacion();
+                expect(puntuacion).to.equal(3.3);
+            });
+        });
+    });    
 
+    describe('calificar', function(){
+        var restaurante; 
+        beforeEach(() => {
+            restaurante = new Restaurant(
+                20,
+                "Pappelli", 
+                "Pizza",
+                "París", 
+                ["12:00", "15:00", "17:30"],
+                 "../img/pizza3.jpg", 
+                 [8, 4, 6, 7, 7, 9, 1]);
+        });
+        describe(' con 5 se obtienen las siguientes calificaciones ',function(){
+            it('[8, 4, 6, 7, 7, 9, 1, 5]', function(){
+                var puntuacionesEsperadas = [8, 4, 6, 7, 7, 9, 1, 5];
+                restaurante.calificar(5);
+                expect(restaurante.calificaciones).to.eql(puntuacionesEsperadas);
+            });
         });
     });    
 });
-
-// describe('Funcion reservarHorario(horario)', function(){
-    
-//     it('Cuando se reserva un horario de un restaurant, la cantidad de horarios disponibles disminuye en uno.', function(){
-//         var restaurante = new Restaurant(4, "Bleecker Street Pizza", "Pizza", "Nueva York", ["12:00", "15:00", "17:30"], "../img/pizza2.jpg", [8, 9, 9, 4, 6, 7]);
-//         var cantHorariosAntesReserva = restaurante.horarios.length;
-//         restaurante.reservarHorario("12:00");
-//         expect(restaurante.horarios.length).to.equal(cantHorariosAntesReserva - 1);
-//     });
-
-//     it('Cuando se reserva un horario que el restaurant no posee, el arreglo no modifica su cantidad de elementos.', function(){
-//         var restaurant = new Restaurant(9, "La Trottinette", "Pasta", "París", ["16:00", "18:00", "21:30"], "../img/pasta5.jpg", [8, 8, 7, 7, 7, 7]);
-//         var cantHorariosAntesReserva = restaurant.horarios.length;
-//         restaurant.reservarHorario("11:00");
-//         expect(restaurant.horarios.length).to.equal(cantHorariosAntesReserva);
-//     });
-
-//     it('Cuando se reserva un horario que el restaurant no posee, el arreglo se mantiene igual.', function(){
-//         var restaurant = new Restaurant(9, "La Trottinette", "Pasta", "París", ["16:00", "18:00", "21:30"], "../img/pasta5.jpg", [8, 8, 7, 7, 7, 7]);
-//         var horariosIniciales = restaurant.horarios;
-//         restaurant.reservarHorario("11:00");
-//         expect(restaurant.horarios).to.be.equal(horariosIniciales);
-//     });
-
-//     it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.', function(){
-//         var restaurant = new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [9, 4, 6, 5, 6]);
-//         var horariosIniciales = restaurant.horarios;
-//         restaurant.reservarHorario();
-//         expect(restaurant.horarios).to.equal(horariosIniciales);
-//     });
-    
-//     it('Cuando se intenta reservar un horario pero no se le pasa ningún parámetro a la función, el arreglo mantiene la cantidad de horarios que tenia antes de la reserva.', function(){
-//         var restaurant = new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [9, 4, 6, 5, 6]);
-//         var cantHorariosIniciales = restaurant.horarios.length;
-//         restaurant.reservarHorario();
-//         expect(restaurant.horarios.length).to.equal(cantHorariosIniciales);
-//     })
-// });
-
-// describe('Función obtenerPuntuacion()', function(){
-//     it('Cuando las calificaciones de un restaurant son [1,2,3,4,5] la puntuacion es 3', function(){
-//         var restaurant = new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [1,2,3,4,5]);
-//         var puntuacion = restaurant.obtenerPuntuacion();
-//         expect(puntuacion).to.equal(3);
-//     })
-//     it('Cuando un restaurante no tiene calificaciones[] la puntuacion es 0',function(){
-//         var restaurant = new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", []);
-//         var puntuacion = restaurant.obtenerPuntuacion();
-//         expect(puntuacion).to.equal(0);
-//     })
-//     it('Cuando un restaurante tiene calificaciones[3,3,4] la puntuacion es 0',function(){
-//         var restaurant = new Restaurant(10, "New London Cafe", "Desayuno", "Londres", ["12:00", "13:00", "14:30"], "../img/desayuno3.jpg", [3,3,4]);
-//         var puntuacion = restaurant.obtenerPuntuacion();
-//         expect(puntuacion).to.equal(3.33);
-//     })
-    
-
-// })
---
+++ readme.md
@@ -0,0 +1,9 @@
+**Reservando**  
+
+Reservando es un proyecto cuyo objetivo fue poner en práctica los conceptos de refactoring, testing y TDD. Realice la construccion de pruebas que validan el funcionamiento de la aplicación y  ayudan a detectar errores en ella. Luego de tener esas pruebas, se refactorizo el código del proyecto
+
+- [x] TDD
+- [x] Unit Test
+- [x] Mocha y Chai
+- [x] Refactoring
+
